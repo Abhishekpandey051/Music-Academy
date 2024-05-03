afea088fc89e740255dac4170e95e5e26b5f55a4
Abhishekpandey051 <95233418+Abhishekpandey051@users.noreply.github.com>
2024-05-03 22:29:02 +0530

    Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       
     </main>
   );
